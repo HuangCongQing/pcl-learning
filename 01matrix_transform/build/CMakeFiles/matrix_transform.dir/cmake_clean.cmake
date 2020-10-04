7b40a95b90efda78a26e4ac2fb6f982d2a4a9adc
HuangCongQing <1756260160@qq.com>
2020-10-04 19:16:35 +0800

    add: 矩阵变换点云

---
+++ 01matrix_transform/build/CMakeFiles/matrix_transform.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/matrix_transform.dir/matrix_transform.cpp.o"
+  "matrix_transform.pdb"
+  "matrix_transform"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/matrix_transform.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
