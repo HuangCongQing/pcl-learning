bb417dd8424483304c15faaf4f48f98d3e85eb97
HuangCongQing <1756260160@qq.com>
2020-10-04 18:35:10 +0800

    del

---
+++ 00pcd_write/pcd_write.cpp
@@ -0,0 +1,37 @@
+/*
+ * @Description: 
+ * @Author: HCQ
+ * @Company(School): UCAS
+ * @Date: 2020-10-04 18:33:16
+ * @LastEditors: HCQ
+ * @LastEditTime: 2020-10-04 18:33:29
+ */
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+
+int main(int argc, char **argv)
+{
+    pcl::PointCloud<pcl::PointXYZ> cloud;
+
+    // Fill in the cloud data
+    cloud.width = 5;
+    cloud.height = 1;
+    cloud.is_dense = false;
+    cloud.points.resize(cloud.width * cloud.height);
+
+    for (auto &point : cloud)
+    {
+        point.x = 1024 * rand() / (RAND_MAX + 1.0f);
+        point.y = 1024 * rand() / (RAND_MAX + 1.0f);
+        point.z = 1024 * rand() / (RAND_MAX + 1.0f);
+    }
+
+    pcl::io::savePCDFileASCII("test_pcd.pcd", cloud);
+    std::cerr << "Saved " << cloud.size() << " data points to test_pcd.pcd." << std::endl;
+
+    for (const auto &point : cloud)
+        std::cerr << "    " << point.x << " " << point.y << " " << point.z << std::endl;
+
+    return (0);
+}
