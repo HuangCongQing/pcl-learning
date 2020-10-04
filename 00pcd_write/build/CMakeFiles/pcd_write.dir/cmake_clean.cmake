36646117e785e00acdd1be5632aae2d2e56cfa93
HuangCongQing <1756260160@qq.com>
2020-10-04 18:53:14 +0800

    add: 编译

---
+++ 00pcd_write/build/CMakeFiles/pcd_write.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pcd_write.dir/pcd_write.cpp.o"
+  "pcd_write.pdb"
+  "pcd_write"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pcd_write.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
