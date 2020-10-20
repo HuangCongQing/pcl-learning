70758aebd4ee03bafcdf039f61fadd8642dd184c
HuangCongQing <1756260160@qq.com>
2020-10-20 15:57:31 +0800

    add: 判断不同参数下不同代码

--- CodeSnippets代码片段/snippets.cpp
+++ CodeSnippets代码片段/snippets.cpp
@@ -4,10 +4,21 @@
  * @Company(School): UCAS
  * @Email: 1756260160@qq.com
  * @Date: 2020-10-20 10:18:35
- * @LastEditTime: 2020-10-20 10:22:51
+ * @LastEditTime: 2020-10-20 15:57:18
  * @FilePath: /pcl-learning/CodeSnippets代码片段/snippets.cpp
  */
 
+// 判断不同参数下不同代码
+  if (strcmp(argv[1], "-r") == 0){     
+    // code
+  }
+  else if (strcmp(argv[1], "-c") == 0){  
+    // code
+  }
+  else{
+    std::cerr << "please specify command line arg '-r' or '-c'" << std::endl;
+    exit(0);
+  }
 
 // 获取参数
 // 附带参数读取点云文件
