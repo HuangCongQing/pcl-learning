5fa26225223a3d27df883258c38b99a608b8705b
HuangCongQing <1756260160@qq.com>
2020-10-20 10:23:04 +0800

    add: 附带参数读取点云文件

--- CodeSnippets代码片段/snippets.cpp
+++ CodeSnippets代码片段/snippets.cpp
@@ -4,8 +4,8 @@
  * @Company(School): UCAS
  * @Email: 1756260160@qq.com
  * @Date: 2020-10-20 10:18:35
- * @LastEditTime: 2020-10-20 10:22:14
- * @FilePath: /pcl-learning/CodeSnippets代码片段/smippets.cpp
+ * @LastEditTime: 2020-10-20 10:22:51
+ * @FilePath: /pcl-learning/CodeSnippets代码片段/snippets.cpp
  */
 
 
