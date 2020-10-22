a90efab7206ed1123adf230e2e143221bb81bfe4
HuangCongQing <1756260160@qq.com>
2020-10-22 10:33:17 +0800

    add: 输入输出点云文件

--- CodeSnippets代码片段/snippets.cpp
+++ CodeSnippets代码片段/snippets.cpp
@@ -4,11 +4,24 @@
  * @Company(School): UCAS
  * @Email: 1756260160@qq.com
  * @Date: 2020-10-20 10:18:35
- * @LastEditTime: 2020-10-20 15:57:18
+ * @LastEditTime: 2020-10-22 10:33:08
  * @FilePath: /pcl-learning/CodeSnippets代码片段/snippets.cpp
  */
 
-// 判断不同参数下不同代码
+// 3 输入输出点云文件
+  // 方式1：
+    pcl::io::loadPCDFile("xxxx.pcd", *cloud);
+    pcl::io::savePCDFileASCII("xxx.pcd", cloud); //保存出错
+  // 方式2：
+    pcl::PCDReader reader;
+    reader.read ("../xxx.pcd", *cloud);
+
+    pcl::PCDWriter writer;
+    writer.write ("../xxx.pcd", *cloud_hull, false);
+
+
+
+// 2 判断不同参数下不同代码
   if (strcmp(argv[1], "-r") == 0){     
     // code
   }
@@ -20,7 +33,9 @@
     exit(0);
   }
 
-// 获取参数
+// 1 获取参数
 // 附带参数读取点云文件
     pcl::PointCloud<PointT>::Ptr cloud(new pcl::PointCloud<PointT>);
     pcl::io::loadPCDFile(argv[1], *cloud);
+
+
