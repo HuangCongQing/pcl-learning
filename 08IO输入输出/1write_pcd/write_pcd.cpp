daba2618fd9c291b0a01dd612989c441121b5844
HuangCongQing <1756260160@qq.com>
2020-10-27 16:26:49 +0800

    update

--- 08IO输入输出/1write_pcd/write_pcd.cpp
+++ 08IO输入输出/1write_pcd/write_pcd.cpp
@@ -1,3 +1,12 @@
+/*
+ * @Description: 写入数据
+ * @Author: HCQ
+ * @Company(School): UCAS
+ * @Email: 1756260160@qq.com
+ * @Date: 2020-10-09 10:42:31
+ * @LastEditTime: 2020-10-27 16:24:56
+ * @FilePath: /pcl-learning/08IO输入输出/1write_pcd/write_pcd.cpp
+ */
 #include <iostream>          //标准C++库中的输入输出的头文件
 #include <pcl/io/pcd_io.h>   //PCD读写类相关的头文件
 #include <pcl/point_types.h> //PCL中支持的点类型的头文件
@@ -28,7 +37,7 @@ int main(int argc, char **argv)
         cloud.points[i].z = 1024 * rand() / (RAND_MAX + 1.0f);
     }
     //把PointCloud对象数据存储在 test_pcd.pcd文件中
-    pcl::io::savePCDFileASCII("test_pcd.pcd", cloud);
+    pcl::io::savePCDFileASCII("../test_pcd.pcd", cloud);
 
     //打印输出存储的点云数据
     std::cerr << "Saved " << cloud.points.size() << " data points to test_pcd.pcd." << std::endl;
