907ef7702f8c5b850d91c60dc51603365b1b4db3
hcq <1756260160@qq.com>
2023-02-06 00:31:16 +0800

    add: 03lidar2img

---
+++ practice/03lidar2img/01PointXYZlidar2image.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <pcl/filters/passthrough.h>
+#include <pcl/point_cloud.h>
+#include <pcl/range_image/range_image.h>
+#include <vector>
+#include <pcl/visualization/common/float_image_utils.h>
+#include <pcl/io/png_io.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_cloud.h>
+#include <pcl/common/transforms.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/console/time.h>
+#include <pcl/point_types.h>
+#include <pcl/common/common.h>
+
+#include <opencv2/highgui.hpp>
+#include <opencv2/core/types.hpp>
+#include <opencv2/core/core.hpp>
+
+using namespace std;
+using namespace cv;
+
+
+int main(int argc, char** argv)
+{
+	 pcl::PointCloud<pcl::PointXYZ> pointcloud;
+	//读入点云
+	if (pcl::io::loadPCDFile("/home/hcq/data/01project/wire_dataset/20221221new_file.pcd", pointcloud) < 0)
+	{
+		PCL_ERROR("目标文件不存在！\n");
+		return -1;
+	}
+	
+	//去除nan点
+	std::vector<int> mapping;
+	pcl::removeNaNFromPointCloud(pointcloud, pointcloud, mapping);
+	//获取点云最值
+	pcl::PointXYZ min;//用于存放三个轴的最小值
+	pcl::PointXYZ max;//用于存放三个轴的最大值
+	pcl::getMinMax3D(pointcloud, min, max);
+
+	cout << "min.x = " << min.x << "\n" << endl;
+	cout << "max.x = " << max.x << "\n" << endl;
+	cout << "min.y = " << min.y << "\n" << endl;
+	cout << "max.y = " << max.y << "\n" << endl;
+	cout << "min.z = " << min.z << "\n" << endl;
+	cout << "max.z = " << max.z << "\n" << endl;
+
+	//定义图像的宽高
+	int image_rows = max.x - min.x+1 ;
+	int image_cols = max.y - min.y +1;
+
+    cv::Mat image1(image_rows, image_cols, CV_8UC1);
+
+	//初始化图像像素为255
+	for (int i = 0; i < image1.rows; i++)
+	{
+		for (int j = 0; j < image1.cols; j++)
+		{
+			image1.at<uchar>(i, j) = 0;	
+		}
+	}
+	//根据点云高度对图像进行赋值
+	for (int i = 0; i < pointcloud.points.size(); i++)
+	{    
+		int image_i, image_j;
+		
+		
+		if (pointcloud.points[i].x < 0)
+		{
+			 image_i = - min.x + pointcloud.points[i].x ;
+		}
+		else 
+		{
+			 image_i = - min.x + pointcloud.points[i].x; //
+		}
+		if (pointcloud.points[i].y < 0)
+	    {
+             image_j = - min.y + pointcloud.points[i].y;
+		}
+		else
+		{
+			 image_j = - min.y + pointcloud.points[i].y;
+		}
+		if( (pointcloud.points[i].z > 0)|| (pointcloud.points[i].z < 0)|| (pointcloud.points[i].z == 0))
+		{
+			if (pointcloud.points[i].z - min.z < 255)
+			{
+				//根据点云高度，进行0-255转换
+				image1.at<uchar>(image_i, image_j) = uchar((pointcloud.points[i].z - min.z) / (max.z-min.z)*255);
+				
+			}
+		}
+		else
+		{
+			image1.at<uchar>(image_i, image_j) = 0;
+		}	
+	}
+	//对图像像素点进行打印
+	std::cout << image1 << std::endl;
+	// imwrite("0-255_level.bmp", image1);
+	imwrite("0-255_level.png", image1);
+	imshow("生成图像", image1);
+	waitKey(0);
+	return (0);
+}
---
+++ practice/03lidar2img/02PointXYZRGBlidar2image.cpp
@@ -0,0 +1,149 @@
+#include <pcl/visualization/pcl_visualizer.h>
+#include <iostream>
+#include <vtkPlaneSource.h>  
+#include <pcl/common/common.h>
+#include<opencv2/opencv.hpp>
+
+
+#include <pcl/io/pcd_io.h>   //PCD读写类相关的头文件
+#include <pcl/point_types.h> //PCL中支持的点类型的头文件
+
+using namespace std;
+
+typedef pcl::PointXYZ point;
+typedef pcl::PointXYZRGB pointcolor;
+
+
+//将a值缩放到0-255之间，放回的类型是整型int
+int scale_to_255(int pixel_value, int min, int max) {
+	return int((((pixel_value - min) / float(max - min)) * 255));
+}
+
+void use_pcdTopicture_main(pcl::PointCloud< pcl::PointXYZRGB>::Ptr cloud, float res, float side_range_left, float side_range_right, float fwd_range_behind , float fwd_range_front, float height_range_min,float height_range_max) {
+	/*初始化 
+	cloud        表示点云地址
+	res          表示分辨率
+	side_range   left - most to right - most
+	fwd_range    back - most to forward - most
+	height_range bottom - most to upper - most*/
+	res = 1.0;
+	//获取点云的最值
+	pcl::PointXYZRGB min, max;
+	pcl::getMinMax3D(*cloud, min, max);
+	//将点云的最值设置为框住点云的边界,自动设置边框大小,目的就是构建图片大小。
+	//在cloudcompare中绿色坐标轴代表y，红色坐标轴代表x，蓝色坐标轴代表z，因此我们想要的是水平面也就是XOZ面
+	//根据双目成像原理我认为点云的原点就是相机的位置。，所以我认为款选的是要固定的
+	float side_range [] = { -1500,800 };
+	float fwd_range[] = { -500 ,500 };
+	float height_range[] = { -2,2};//y轴相关
+	//记录筛选出来的点云下标
+	vector<int>indices;
+
+	//获取用矩形框款住想要的点云 利用循环每一个点云，筛选出属于规定框中的点云
+	for (int i = 0; i < cloud->points.size(); i++) {
+		if ((cloud->points[i].x > fwd_range[0] && cloud->points[i].x < fwd_range[1]) && (cloud->points[i].z > side_range[0] && cloud->points[i].z < side_range[1]))
+		{
+			indices.push_back(i);
+		}
+	};
+
+	//在img图像中坐标
+	vector<int>x_img;
+	vector<int>y_img;
+	//将点云的值映射到像素位置中,分辨率为1的话则，将点云的值原封不动的映射到图像中
+	for (int i = 0; i < indices.size(); i++) {
+		x_img.push_back(-cloud->points[i].z/res);
+		y_img.push_back(-cloud->points[i].x / res);
+
+	}
+	//平移图像数据到点云的最左上角
+	for (int i = 0; i < indices.size(); i++) {
+		x_img[i]-=int(floor(side_range[0]/res));
+		y_img[i] += int(ceil(fwd_range[1] / res));
+
+	}
+	//将厚度信息的值转化为255值，并且填充到对应的img中对应的坐标中
+	vector<int>pixel_values;
+	//先筛选符合厚度条件的点云
+	for (int i = 0; i < indices.size(); i++) {
+		if (cloud->points[i].y > height_range[0] && cloud->points[i].y < height_range[1])
+		{
+			pixel_values.push_back(cloud->points[i].y);
+		}
+	}
+	//将点云的厚度信息经过比例转化成255对应值，赋值到img图像对应的坐标值中
+	for (int i = 0; i < indices.size(); i++) {
+		pixel_values[i]=scale_to_255(pixel_values[i], height_range[0], height_range[1]);
+	}
+
+	//创建图像大小，取决于我们所定义的框选矩形和分辨率
+	int x_max = 1 + int((side_range[1] - side_range[0]) / res);
+	int y_max = 1 + int((fwd_range[1] - fwd_range[0]) / res);
+	//利用opencv创建一个Mat类型来存储图像的信息
+	cv::Mat im = cv::Mat::zeros(x_max, y_max, CV_8U);
+	//im[x_img, y_img]=pixel_values;
+	//创建一个循环，遍历x_img或者y_img的点，给Mat类型的图像，对应的x_img，y_img坐标赋值pixel_values值
+	for (int i = 0; i < x_img.size(); i++) {
+		im.at<uchar>(x_img[i], y_img[i]) = pixel_values[i];
+	}
+	//数据图片打印信息
+	cout << "图像中x轴最小点坐标值为x_img min:"<< *min_element(x_img.begin(), x_img.end()) << endl;
+	cout << "图像中x轴最大点坐标值为x_img max:" << *max_element(x_img.begin(), x_img.end()) << endl;
+	cout << "图像中y轴最小点坐标值为y_img min:" << *min_element(y_img.begin(), y_img.end()) << endl;
+	cout << "图像中y轴最大点坐标值为y_img max:" << *max_element(y_img.begin(), y_img.end()) << endl;
+	
+	
+
+	//可视化
+	cvNamedWindow("img_result");
+	cv::imshow("img_result", im);
+
+	//取反
+	cv::Mat im_reverse;
+	im_reverse = 255 - im;
+	cvNamedWindow("img_reverse_result");
+	cv::imshow("img_reverse_result", im_reverse);
+	cvvWaitKey();
+
+	//将图片保存
+	std::vector<int>param;
+	param.push_back(CV_IMWRITE_PXM_BINARY);
+	cv::imwrite("img_reverse_result.bmp", im_reverse, param);
+	cout << "save img" << endl;
+	
+}
+
+
+
+
+int main(int argc,char **argv)
+{
+    
+    pcl::PointCloud<pointcolor>::Ptr input (new pcl::PointCloud<pointcolor>);
+    // pcl::io::loadPCDFile(argv[1],*input);
+    pcl::io::loadPCDFile("/home/hcq/data/01project/wire_dataset/20221221new_file.pcd",*cloud);
+
+
+
+
+    // 
+    /*初始化 
+	cloud        表示点云地址
+	res          表示分辨率
+	side_range   left - most to right - most
+	fwd_range    back - most to forward - most
+	height_range bottom - most to upper - most
+    */
+    float res = 0.05;
+    float side_range_left = -100;
+    float side_range_right = 100;
+
+    float fwd_range_behind = -100;
+    float fwd_range_front = 100;
+
+    float height_range_min = -100;
+    float height_range_max = 100;
+    use_pcdTopicture_main(cloud, res, side_range_left, side_range_right, fwd_range_behind, fwd_range_front, height_range_min, height_range_max);
+
+    return 0;
+}
